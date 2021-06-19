c5c37976a5fa424388be564818551aeaab61794c
Max Bowman <maxjbowman@gmail.com>
2021-06-19 14:31:23 +0100

    Added Testing python module Simple script, just for development purposes. Uses python turtle graphics to display a set of points (so that I can see if they have been decoded from an SVG correctly).
    Signed-off-by: Max Bowman <maxjbowman@gmail.com>

---
+++ pythontests/basicviewer.py
@@ -0,0 +1,15 @@
+#bad python code because i wanted to test some stuff
+import fileinput
+import turtle
+drawturtle = turtle.Turtle()
+drawturtle.penup()
+y = 1
+for x in fileinput.input():
+    print(y,x)
+    x = x.split(",")
+    a = float(x[0])
+    b = float(x[1])
+    drawturtle.goto(a,b)
+    drawturtle.pendown()
+    y += 1
+input()
