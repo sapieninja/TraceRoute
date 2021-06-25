d8cd6d59552faadab1f5a42384ad75eadb37ff95
Max Bowman <maxjbowman@gmail.com>
2021-06-25 11:54:41 +0100

    Added start of fitness functions for collaboration
    Signed-off-by: Max Bowman <maxjbowman@gmail.com>

---
+++ app/src/main/java/TraceRoute/fitness/Fitness.java
@@ -0,0 +1,14 @@
+package TraceRoute.fitness;
+
+
+import java.util.List;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+public class Fitness {
+    public static Double Perpendicular(Graph london, List<List> Points,double x, double y, double scale)
+    {
+
+    }
+}
